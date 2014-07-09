7009cb34fb0c7b7bf61e01c12bbf919ceb4a3038
Ryan Wahle <ryanwahle@fullsail.edu>
2014-07-09 14:50:08 -0700

    video player functionality complete

--- VideoAnalyzer/app/src/main/java/com/ryanwahle/videoanalyzer/PlayerActivity.java
+++ VideoAnalyzer/app/src/main/java/com/ryanwahle/videoanalyzer/PlayerActivity.java
@@ -1,9 +1,12 @@
 package com.ryanwahle.videoanalyzer;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 
 public class PlayerActivity extends Activity {
@@ -12,25 +15,19 @@ public class PlayerActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
-    }
 
+        //String videoURLString = "android.resource://com.ryanwahle.videoanalyzer/" + R.raw.testvideo;
+        String videoURLString = "https://archive.org/download/ksnn_compilation_master_the_internet/ksnn_compilation_master_the_internet_512kb.mp4";
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.player, menu);
-        return true;
-    }
+        VideoView playerVideoView = (VideoView) findViewById(R.id.playerVideoView);
+        playerVideoView.setVideoURI(Uri.parse(videoURLString));
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        MediaController playerMediaController = new MediaController(this);
+        playerMediaController.setAnchorView(playerVideoView);
+
+        playerVideoView.setMediaController(playerMediaController);
+
+        playerVideoView.start();
+
     }
 }
